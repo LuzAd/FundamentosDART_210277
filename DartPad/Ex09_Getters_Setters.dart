acb0cf077ec75fcd404e08c05e4fc9df627ac42e
LuzAd <lreyesgonzalez94@gmail.com>
2024-10-02 14:12:55 -0600

    Practica 11
    Agregación de la practica 11: Getters y setters

---
+++ DartPad/Ex09_Getters_Setters.dart
@@ -0,0 +1,29 @@
+void main() {
+ final mySquare = Square(side:10);
+  mySquare._side= -5;
+
+  print("""área: ${mySquare._side}
+    Área que ocupa :${mySquare.calculateArea()}""");
+}
+
+class Square{
+
+  double _side; // side * side 
+  Square({ required double side})
+    :_side=side;
+
+  double get area {
+    return _side * _side;
+  }
+
+  set side (double value){
+    print("setting new value $value");
+    if( value < 0) throw "Value must be >=0";
+
+    _side= value;
+  }
+
+  double calculateArea(){
+    return _side* _side;
+  }
+}
